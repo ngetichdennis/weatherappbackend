6af1a9518769d629ac97780e96956c609498a2e3
Dennis <ngetichdennis90@gmail.com>
2025-04-24 11:34:31 +0300

    modified the cotrollers to use he data from the weather api for forecasting

--- app/Http/Controllers/API/WeatherController.php
+++ app/Http/Controllers/API/WeatherController.php
@@ -30,13 +30,49 @@ class WeatherController extends Controller
             'city'=>$data['name'],
             'temperature'=>round($data['main']['temp']),
             'weather'=>$data['weather'][0]['main'],
-            'icon'=>$data['weather'][0][icon],
-            'date'=>now()->format('js F Y'),
+            'icon'=>$data['weather'][0]['icon'],
+            'date' => now()->format('jS F Y'),
             'wind_speed'=>$data['wind']['speed'],
             'wind_direction'=>$this->convertDegreesToDirection($data['wind']['deg']),
             'humidity'=>$data['main']['humidity']
         ]);
 
     }
+    // getting 3 days weather forecast
+    public function getForecast(Request $request)
 
+    {
+        $city = $request-> query('city','Nairobi');
+        $units =$request -> query ('units','metric');
+        $geo=$this -> getCoordinates($city);
+        if (!$geo){
+            return response()->json(['error'=> 'Invalid city name'],400);
+        }
+        [$lat,$lon]=$geo;
+        $response = Http::get("https://api.openweathermap.org/data/2.5/forecast",[
+            'lat'=>$lat,
+            'lon'=>$lon,
+            'appid'=>$this->apiKey,
+            'units'=>$units,
+        ]);
+        if ($response->failed()){
+            return response()-> json(['error'=>'could not fetch forecast data'],500);
+        }
+        $data =$response->json();
+        $daily=[];
+        foreach ($data['list'] as $forecast){
+            $date = date ('Y-m-d',strtotime($forecast['dt_txt']));
+            if (!isset($daily[$date])){
+                $daily[$date]=[
+                    'date'=>$date,
+                    'min_temp'=>$forecast['main']['temp_min'],
+                    'max_temp'=>$forecast['main']['temp_max'],
+                    'icon'=>$forecast['weather'][0]['icon'],
+                    'description' => $forecast['weather'][0]['main']
+
+                ];
+            }
+        }
+        return response()->json(array_values(array_slice($daily,1,3)));
+    }
 }
